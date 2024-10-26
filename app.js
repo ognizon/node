3fe75326b1683aeeecff44c6b6b052a815a0ba8b
ognizon <tech4all@gmail.com>
2024-10-26 11:56:39 +0530

    mail

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Create Node Server
 var http = require('http');
+var dt = require('./mymodule');
 
 http.createServer(function (req, res)  { 
+  //Server message in terminal
+console.log('Server is running at http://localhost:8080/');
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
   res.end('Hello World!');
 }).listen(8080);
