f2d2b37eb99bfaa8820741094add7a4541a9467c
ognizon <tech4all@gmail.com>
2024-10-28 20:40:44 +0530

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var dt = require('./mymodule');
 
 http.createServer(function (req, res)  { 
   //Server message in terminal
+  console.log('Welcome');
 console.log('Server is running at http://localhost:8080/');
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
